484d4e5498158170c6411f43a57cbe88d04201dc
yaoyixin <yixin.yao@schinper.com>
2021-08-04 18:16:49 +0800

    ~

---
+++ build/CMakeFiles/yolov5.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yolov5.dir/yolov5.cpp.o"
+  "CMakeFiles/yolov5.dir/yolov5.cpp.o.d"
+  "yolov5"
+  "yolov5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yolov5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
